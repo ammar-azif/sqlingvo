ffc9d3fca585a9f1ae13bd47855f0e0cc80f6402
Roman Scherer <roman.scherer@burningswell.com>
2014-04-02 21:50:28 +0200

    More EBNF ...

--- src/sqlingvo/parser.clj
+++ src/sqlingvo/parser.clj
@@ -17,3 +17,59 @@
     expression = #'[a-zA-Z0-9]+'
     output-name = #'[a-zA-Z0-9]+'"
    :auto-whitespace whitespace))
+
+(def postgresql-parser
+  (insta/parser
+   "select-stmt = select-no-parens | select-with-parens
+    select-with-parens = '(' select-no-parens ')' | '(' select-with-parens ')'
+    select-no-parens = simple-select | select-clause sort-clause
+    select-clause = simple-select | select-with-parens
+    simple-select = 'SELECT' opt-distinct target-list into-clause from-clause where-clause group-clause having-clause window-clause
+    opt-distinct = 'DISTINCT' | 'DISTINCT ON' '(' expr-list ')' | 'ALL'
+    expr-list = a-expr | expr-list ',' a-expr
+    a-expr = c-expr
+      | a-expr 'TYPECAST' typename
+      | a-expr 'COLLATE' any-name
+      | a-expr 'AT TIME ZONE' a-expr
+      | '+' a-expr
+      | '-' a-expr
+      | a-expr '+' a-expr
+      | a-expr '-' a-expr
+      | a-expr '*' a-expr
+      | a-expr '/' a-expr
+      | a-expr '%' a-expr
+      | a-expr '^' a-expr
+      | a-expr '<' a-expr
+      | a-expr '>' a-expr
+      | a-expr '=' a-expr
+      | a-expr qual-op a-expr
+      | qual-op a-expr
+      | a-expr qual-op
+      | a-expr 'AND' a-expr
+      | a-expr 'OR' a-expr
+      | 'NOT' a-expr
+      | a-expr 'LIKE' a-expr
+      | a-expr 'LIKE' a-expr 'ESCAPE' a-expr
+      | a-expr 'NOT LIKE' a-expr
+      | a-expr 'NOT LIKE' a-expr 'ESCAPE' a-expr
+      | a-expr 'ILIKE' a-expr
+      | a-expr 'ILIKE' a-expr 'ESCAPE' a-expr
+      | a-expr 'NOT ILIKE' a-expr
+      | a-expr 'NOT ILIKE' a-expr 'ESCAPE' a-expr
+      | a-expr 'SIMILAR TO' a-expr
+      | a-expr 'SIMILAR TO' a-expr 'ESCAPE' a-expr
+      | a-expr 'NOT SIMILAR TO' a-expr
+      | a-expr 'NOT SIMILAR TO' a-expr 'ESCAPE' a-expr
+    qual-op =  #'.*'
+    any-name = #'.*'
+    c-expr = #'.*'
+    typename = #'.*'
+    target-list = #'.*'
+    into-clause = #'.*'
+    from-clause = #'.*'
+    where-clause = #'.*'
+    group-clause = #'.*'
+    having-clause = #'.*'
+    sort-clause = #'.*'
+    window-clause = #'.*'"
+   :auto-whitespace whitespace))
